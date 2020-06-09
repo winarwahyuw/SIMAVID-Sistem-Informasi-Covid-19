9aa466e198933e702c35ec830405585890dbf78f
winarwahyuw <winarwahyuw99@gmail.com>
2020-06-09 11:03:02 +0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style/main.css';
-import './script/gradient-banner.js';
-import './script/card-banner.js';
-import main from './script/main.js';
+import './script/components/top-nav.js';
+import './script/components/gradient-banner.js';
+import './script/components/card-banner.js';
 
+import './script/components/search-bar.js'
+import main from './script/main.js';  
 main();
