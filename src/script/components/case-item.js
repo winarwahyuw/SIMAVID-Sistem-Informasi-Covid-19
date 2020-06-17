06a527c46d66bc884e2fc3be2d7ce1bf4de474de
winarwahyuw <winarwahyuw99@gmail.com>
2020-06-17 22:41:35 +0700

    perbaikan argument

--- src/script/components/case-item.js
+++ src/script/components/case-item.js
@@ -1,36 +1,36 @@
 class CaseItem extends HTMLElement{
-    set confirmedValue(params){
-        this._params=params;
+    set confirmedValue(confirmed){
+        this._confirmed=confirmed;
         this.innerHTML=`
         <dl class="row mb-0">
         <dt class="col-4">Confirmed</dt>
         <dd class="col-8">
-            <p class="progress-bar bg-danger">${this._params.value}</p>
+            <p class="progress-bar bg-danger">${this._confirmed.value}</p>
         </dd>
         </dl>
         
     `;
     }
 
-    set recoveredValue(params){
-        this._params=params;
+    set recoveredValue(recovered){
+        this._recover=recovered;
         this.innerHTML=`
         <dl class="row mb-0">
         <dt class="col-4">Recovered</dt>
         <dd class="col-8">
-            <p class="progress-bar bg-warning">${this._params.value}</p>
+            <p class="progress-bar bg-warning">${this._recover.value}</p>
         </dd>
         </dl>
     `;
     }
 
-    set deathValue(params){
-        this._params=params;
+    set deathValue(death){
+        this._death=death;
         this.innerHTML=`
         <dl class="row mb-0">
         <dt class="col-4">Deaths</dt>
         <dd class="col-8">
-            <p class="progress-bar bg-dark">${this._params.value}</p>
+            <p class="progress-bar bg-dark">${this._death.value}</p>
         </dd>
         </dl>
     `;
